d5d0c8dc42377498a6044816377fb1aae04d382a
blake-wen <44958595+blake-wen@users.noreply.github.com>
2018-12-17 17:42:08 +0800

    Revert "Revert "Initial commit""
    This reverts commit 851134356130e496932df62854c391250aff330d.

---
+++ src/github_test.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : github_test.c
+ Author      : bbb
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
